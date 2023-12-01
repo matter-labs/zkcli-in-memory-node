6d5714102fcd0c2be28fcd32bee3ff259e88425b
Jack Hamer <jackhamer0990@gmail.com>
2023-12-01 11:15:34 +0200

    fix: switch rpc url to 127.0.0.1

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ export default class SetupModule extends ModuleNode<ModuleConfig> {
     return {
       l2: {
         chainId: 260,
-        rpcUrl: "http://localhost:8011",
+        rpcUrl: "http://127.0.0.1:8011",
       },
     };
   }
